56cb825092c520460009d49a0b5ee9fad386b119
John Arban Lewis <johnarban@users.noreply.github.com>
2026-04-10 15:08:26 -0400

    including this file was an accident

--- src/composables/useArtemisLocation.ts
+++
@@ -1,7 +0,0 @@
-import { loadHorizonsVectorsForWwt } from "@/horizons";
-import { SolarSystemObjects } from "@wwtelescope/engine-types";
-
-export function useArtemisLocation(
-    url: string,
-    coordCenter: SolarSystemObjects,
-    wwtCenter: SolarSystemObjects,
